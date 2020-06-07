0601086761ccfbf1c38bd2866d27234bf5cf39dd
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-06-08 01:34:19 +0300

    python http.server

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker run -d --rm \
 	-e DISPLAY=${DISPLAY} \
 	-v cifar10_workspace_data:/workspace \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-p 80:80
+	-p 80:80 \
 	--name cifar10_workspace \
 	riscue/cifar10_workspace
 	#/bin/bash -c 'cd ~/pytorch-cifar10 && python main.py -r -m bit'
